c92fd13b68b0a011143f4f5eb055ab519b576cf4
Danbka523 <gordonanet12@gmail.com>
2022-12-21 15:45:56 +0300

    fix dbs

--- db/migrate/20221221124313_create_portfolios.rb
+++ db/migrate/20221221124313_create_portfolios.rb
@@ -1,9 +1,8 @@
 class CreatePortfolios < ActiveRecord::Migration[7.0]
   def change
     create_table :portfolios do |t|
-      t.integer :id
+      t.integer :p_id
       t.integer :user_id
-      t.date :created_at
       t.date :archived_at
       t.integer :currency_id
 
